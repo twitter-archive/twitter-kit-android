a0b81677f1d7a747965ad0d464ab19f0e1972b73
Eric Frohnhoefer <efrohnhoefer@twitter.com>
2017-03-22 20:49:11 -0700

    Copy ActivityLifecycleManager from Fabric
    Change-Id: Ia6fb82e3b1f93e610f79a219d9a0f0a8440c7b2b

---
+++ twitter-core/src/main/java/com/twitter/sdk/android/core/internal/ActivityLifecycleManager.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright (C) 2015 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.twitter.sdk.android.core.internal;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * This is a convenience class that wraps the ActivityLifecycleCallbacks registration. It provides
+ * an abstract Callbacks class that reduces required boilerplate code in your callbacks as well as
+ * OS Version checks that make it compatible with Android versions less than Ice Cream Sandwich.
+ */
+public class ActivityLifecycleManager {
+    private final Application application;
+    private ActivityLifecycleCallbacksWrapper callbacksWrapper;
+
+    /**
+     * Override the methods corresponding to the activity.
+     */
+    public static abstract class Callbacks {
+        public void onActivityCreated(Activity activity, Bundle bundle) {}
+        public void onActivityStarted(Activity activity) {}
+        public void onActivityResumed(Activity activity) {}
+        public void onActivityPaused(Activity activity) {}
+        public void onActivityStopped(Activity activity) {}
+        public void onActivitySaveInstanceState(Activity activity, Bundle bundle) {}
+        public void onActivityDestroyed(Activity activity) {}
+    }
+
+    /**
+     * @param context Any context object, it is not stored
+     */
+    public ActivityLifecycleManager(Context context) {
+        application = (Application) context.getApplicationContext();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            callbacksWrapper = new ActivityLifecycleCallbacksWrapper(application);
+        }
+    }
+
+    /**
+     * @param callbacks The callbacks
+     * @return true if the version of the application context supports registering lifecycle
+     * callbacks
+     */
+    public boolean registerCallbacks(Callbacks callbacks) {
+        return callbacksWrapper != null &&
+                callbacksWrapper.registerLifecycleCallbacks(callbacks);
+    }
+
+    /**
+     * Unregisters all previously registered callbacks on the application context.
+     */
+    public void resetCallbacks() {
+        if (callbacksWrapper != null) {
+            callbacksWrapper.clearCallbacks();
+        }
+    }
+
+    private static class ActivityLifecycleCallbacksWrapper {
+        private final Set<Application.ActivityLifecycleCallbacks> registeredCallbacks =
+                new HashSet<Application.ActivityLifecycleCallbacks>();
+        private final Application application;
+
+        ActivityLifecycleCallbacksWrapper(Application application) {
+            this.application = application;
+        }
+
+        @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+        private void clearCallbacks() {
+            for (Application.ActivityLifecycleCallbacks callback : registeredCallbacks) {
+                application.unregisterActivityLifecycleCallbacks(callback);
+            }
+        }
+
+        @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+        private boolean registerLifecycleCallbacks(final Callbacks callbacks) {
+
+            if (application != null) {
+                final Application.ActivityLifecycleCallbacks callbackWrapper =
+                        new Application.ActivityLifecycleCallbacks() {
+
+                            @Override
+                            public void onActivityCreated(Activity activity, Bundle bundle) {
+                                callbacks.onActivityCreated(activity, bundle);
+                            }
+
+                            @Override
+                            public void onActivityStarted(Activity activity) {
+                                callbacks.onActivityStarted(activity);
+                            }
+
+                            @Override
+                            public void onActivityResumed(Activity activity) {
+                                callbacks.onActivityResumed(activity);
+                            }
+
+                            @Override
+                            public void onActivityPaused(Activity activity) {
+                                callbacks.onActivityPaused(activity);
+                            }
+
+                            @Override
+                            public void onActivityStopped(Activity activity) {
+                                callbacks.onActivityStopped(activity);
+                            }
+
+                            @Override
+                            public void onActivitySaveInstanceState(Activity activity,
+                                    Bundle bundle) {
+                                callbacks.onActivitySaveInstanceState(activity, bundle);
+                            }
+
+                            @Override
+                            public void onActivityDestroyed(Activity activity) {
+                                callbacks.onActivityDestroyed(activity);
+                            }
+                        };
+                application.registerActivityLifecycleCallbacks(callbackWrapper);
+                this.registeredCallbacks.add(callbackWrapper);
+                return true;
+            }
+            return false;
+        }
+    }
+}
