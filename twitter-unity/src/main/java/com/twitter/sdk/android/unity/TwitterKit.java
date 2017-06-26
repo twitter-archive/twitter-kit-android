4960bdaffb889692a1ffea65399c6e60be800dcd
Adam Cummings <adamc@twitter.com>
2017-06-26 14:20:41 -0700

    Update TwitterKit Unity methods
    Change-Id: Icf810bad648ffbfb310b8c515a2e621af7a49bb7

--- twitter-unity/src/main/java/com/twitter/sdk/android/unity/TwitterKit.java
+++ twitter-unity/src/main/java/com/twitter/sdk/android/unity/TwitterKit.java
@@ -19,9 +19,13 @@ package com.twitter.sdk.android.unity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterConfig;
 import com.twitter.sdk.android.core.TwitterCore;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
@@ -30,8 +34,24 @@ import com.twitter.sdk.android.core.identity.TwitterAuthClient;
 import com.twitter.sdk.android.tweetcomposer.ComposerActivity;
 import com.unity3d.player.UnityPlayer;
 
+/**
+ * A class to expose TwitterKit methods for Unity
+ */
 public class TwitterKit {
     public static final String GAME_OBJECT_NAME = "TwitterGameObject";
+
+    /**
+     * Initializes TwitterKit, must be called before {@link #login}
+     * @param consumerKey Twitter App Consumer Key (API Key)
+     * @param consumerSecret Twitter App Consumer Secret (API Secret)
+     */
+    public static void init(final String consumerKey, final String consumerSecret) {
+        final TwitterConfig config = new TwitterConfig.Builder(
+                UnityPlayer.currentActivity.getApplicationContext())
+                .twitterAuthConfig(new TwitterAuthConfig(consumerKey, consumerSecret))
+                .build();
+        Twitter.initialize(config);
+    }
 
     /**
      * Convenience method for launching Twitter login using JNI.
@@ -62,12 +82,17 @@ public class TwitterKit {
      * Convenience method for starting Tweet composer with app card preview.
      *
      * @param session the user session
+     * @param imageUri URI of the image
+     * @param text the text to be included in the tweet
+     * @param hashtags the hashtags to be included in the tweet
      */
-    public static void compose(String session, String[] hashtags) {
+    public static void compose(String session, String imageUri, String text, String[] hashtags) {
         final Activity currentActivity = UnityPlayer.currentActivity;
 
         final Intent intent = new ComposerActivity.Builder(currentActivity)
                 .session(TwitterSessionHelper.deserialize(session))
+                .image(imageUri != null ? Uri.parse(imageUri) : null)
+                .text(text)
                 .hashtags(hashtags)
                 .createIntent();
         currentActivity.startActivity(intent);
